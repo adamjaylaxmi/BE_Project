c0df8fe0c994801f24041d2ac6ff8c5b0b6d1ff4
Jaylaxmi Adam <adamjaylaxmi@gmail.com>
2019-09-12 13:53:44 -0700

    All files  modified that commited

--- MongoConn.js
+++ MongoConn.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-var connString="mongodb+srv://adamjaylaxmi:Jayashri2*@cluster0-tdtig.mongodb.net/test";
+var connString="mongodb+srv://adamjaylaxmi:<PASSWORD>@cluster0-tdtig.mongodb.net/test";
 MongoClient.connect(connString, {useNewUrlParser:true},{useUnifiedTopology:true},function (err, db) {
    
      if(err) throw err;
